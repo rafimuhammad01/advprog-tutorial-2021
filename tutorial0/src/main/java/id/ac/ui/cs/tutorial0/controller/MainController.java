3d3f80cea3cd75b4c43e1687e2e48d904d293e41
rafimuhammad01 <rafi10muhammad@gmail.com>
2021-02-23 19:51:44 +0700

    Update home function return value

--- tutorial0/src/main/java/id/ac/ui/cs/tutorial0/controller/MainController.java
+++ tutorial0/src/main/java/id/ac/ui/cs/tutorial0/controller/MainController.java
@@ -11,7 +11,7 @@ public class MainController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     private String home() {
-        return "home";
+        return "house";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/greet")
