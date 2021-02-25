f7bdacd73b18683f71ef21e75cc8177b58266cfc
rafimuhammad01 <rafi10muhammad@gmail.com>
2021-02-25 19:53:46 +0700

    Revert "implement powerClassifier"
    This reverts commit 547e4c5d88e6ebf809f51c4155912c85073fbf1c.

--- tutorial0/src/main/java/id/ac/ui/cs/tutorial0/controller/AdventurerController.java
+++ tutorial0/src/main/java/id/ac/ui/cs/tutorial0/controller/AdventurerController.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.tutorial0.controller;
 
 import id.ac.ui.cs.tutorial0.service.AdventurerCalculatorService;
-import id.ac.ui.cs.tutorial0.service.powerClassifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,14 +12,21 @@ public class AdventurerController {
 
     @Autowired
     private AdventurerCalculatorService adventurerCalculatorService;
-    @Autowired
-    private powerClassifier powerClassifier;
 
     @RequestMapping("/adventurer/countPower")
     private String showAdventurerPowerFromBirthYear(@RequestParam("birthYear")int birthYear, Model model) {
+        String powerInString = "";
         int power = adventurerCalculatorService.countPowerPotensialFromBirthYear(birthYear);
-        model.addAttribute("power", adventurerCalculatorService.countPowerPotensialFromBirthYear(birthYear));
-        model.addAttribute("class", powerClassifier.powerClassification(power));
+
+        if (power < 20000) {
+            powerInString += power + " C class";
+        } else if (power >= 20000 && power < 100000) {
+            powerInString += power + " B class";
+        } else {
+            powerInString += power + " A class";
+        }
+
+        model.addAttribute("power", powerInString);
 
         return "calculator";
     }
--- tutorial0/src/main/java/id/ac/ui/cs/tutorial0/service/powerClassifier.java
+++
@@ -1,6 +0,0 @@
-package id.ac.ui.cs.tutorial0.service;
-
-public interface powerClassifier {
-    public String powerClassification(int power);
-
-}
